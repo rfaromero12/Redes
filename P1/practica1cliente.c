0beb07b368a6acb8de6c6e49986f97ea2f0ab96c
rfaromero12 <rafael24.07.2001@gmail.com>
2021-10-11 10:14:51 +0200

    Practica 1

---
+++ P1/practica1cliente.c
@@ -0,0 +1,119 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+#include <arpa/inet.h>
+#include <string.h>
+
+int main (int argc, char **argv )
+{
+	if (argc != 3)
+	{
+		printf("Error en la linea de comandos\n");
+		exit(0);
+	}
+
+    //Datos del select
+    struct timeval timeout;
+	fd_set lectura;
+	int salida;
+	char cadena_select[60];
+
+    //Inicializar la estructua timeval
+	timeout.tv_sec = atoi(argv[2]);
+	timeout.tv_usec = 0;
+	
+	//Inicializar los conjuntos fd_set
+	FD_ZERO(&lectura);
+	FD_SET(0,&lectura);
+
+		/*---------------------------------------------------- 
+			Descriptor del socket y buffer para datos 
+		-----------------------------------------------------*/
+		int Socket_Cliente;
+		char Datos[80];
+   
+   	/* -----------------------------------------------------
+   		Informaci\ufffdn del Servidor
+   	-----------------------------------------------------*/
+   	struct sockaddr_in Servidor;  
+   	socklen_t Longitud_Servidor;
+  
+  
+   	/* --------------------------------------------------
+			Se abre el socket cliente
+		---------------------------------------------------*/
+		Socket_Cliente = socket (AF_INET, SOCK_DGRAM, 0);
+		if (Socket_Cliente == -1)
+		{
+			printf ("No se puede abrir el socket cliente\n");
+    			exit (-1);	
+		}
+
+ 		/*---------------------------------------------------------------------
+			Necesitamos una estructura con la informaci\ufffdn del Servidor
+			para poder solicitarle un servicio.
+   		----------------------------------------------------------------------*/
+		Servidor.sin_family = AF_INET;
+		Servidor.sin_port = htons(3160);
+		Servidor.sin_addr.s_addr =  inet_addr(argv[1]);
+  	 	Longitud_Servidor = sizeof(Servidor);
+	
+
+		/*------------------------------------------------------------------------
+			Vamos a preguntarle al usuario sobre el formato del paquete
+		------------------------------------------------------------------------ */
+        printf("Que formato de DAYTIME quieres:\n DAY , TIME o DAYTIME\n");
+        scanf("%s",&Datos);
+    
+
+
+   	
+		/*-----------------------------------------------------------------------
+			Se env\ufffda mensaje al Servidor
+		-----------------------------------------------------------------------*/
+		int enviado = sendto (Socket_Cliente, (char *) &Datos, sizeof(Datos), 0,
+			(struct sockaddr *) &Servidor, Longitud_Servidor);
+   
+   	if (enviado < 0)
+    	{
+			printf("Error al solicitar el servicio\n");
+		}
+    	else
+		{
+             salida = select(1,&lectura,NULL,NULL,&timeout);
+        	if(salida == -1)
+    	{
+	    	printf("Se ha producido un error en select\n");
+    	}
+	    else if(salida == 0)
+	    {
+		    printf("Se ha agotado el tiempo\n");
+
+	    }
+	    else{
+            
+			int recibido = recvfrom (Socket_Cliente, (char *)&Datos, sizeof(Datos), 0,
+			(struct sockaddr *) &Servidor, &Longitud_Servidor);
+			
+   		if (recibido > 0)
+      		printf ("Leido %s\n", Datos);
+   		else
+      		printf ("Error al leer del servidor\n");
+		
+		}
+		    
+            
+            
+        } 
+	
+			
+		
+		close(Socket_Cliente);
+
+	return 0;
+}
---
+++ P1/practica1servidor.c
@@ -0,0 +1,121 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <time.h>
+#include <locale.h>
+
+int main(int args,char **argv){
+    
+	
+	
+    //estructura tiempo
+    time_t tiempo;
+	struct tm * stTm;
+	char cadena_tiempo [80];
+
+
+	tiempo = time(NULL);
+
+	setlocale(LC_ALL,"");
+	
+	stTm = localtime(&tiempo);
+
+	
+
+	
+	
+
+
+
+    //Datos server
+    struct sockaddr_in Servidor;
+    int Descriptor_Servidor;
+
+    //Datos cliente
+    struct sockaddr_in Cliente;
+    socklen_t longitudCliente;
+    //Apertura del socket
+    Descriptor_Servidor = socket(AF_INET,SOCK_DGRAM,0);
+    
+    if (Descriptor_Servidor == -1)
+    {
+        exit(EXIT_FAILURE);
+        printf("Error en la apertura del socket\n");
+    }
+    //Rellenamos la estructura 
+    Servidor.sin_family = AF_INET;
+    Servidor.sin_addr.s_addr = htonl(INADDR_ANY);      //inet_addr(   );
+    Servidor.sin_port = htons(3160);
+
+
+    //Asociamos puerto al descriptor
+    if (bind(Descriptor_Servidor,(struct sockaddr_in *)&Servidor, &longitudCliente) == -1)
+    {
+        printf("No se pudo asociar el socket a un puerto\n");
+        close(Descriptor_Servidor);
+        exit(EXIT_FAILURE);
+    }
+
+
+    longitudCliente = sizeof(Cliente);
+
+    while(1){
+          //recibo los mensajes del cliente
+           char cadena_recibida[80];
+          //Dependiendo del paquete codifico el mensaje, lo codifico y lo devuelvo.  
+          	int recibido = recvfrom (Descriptor_Servidor, &cadena_recibida, sizeof(cadena_recibida), 0,
+			(struct sockaddr *) &Cliente, &longitudCliente);
+
+
+
+		/* -----------------------------------------------------------------
+			Comprobamos si hemos recibido alguna informaci\ufffdn 
+		-------------------------------------------------------------------*/
+		if (recibido > 0)
+		{
+			/*-----------------------------------------------------------------
+				Incrementamos el valor que nos ha enviado el cliente 
+				------------------------------------------------------------------*/
+      	    if (strcmp(cadena_recibida,"DAY") == 0)
+              { 
+                  strftime(cadena_tiempo,80,"%A, %d de %B de %Y", stTm);
+                  printf ("Recibido %s, envio %s\n", cadena_recibida,cadena_tiempo );
+
+              }
+            else if (strcmp(cadena_recibida,"TIME") == 0)
+            {
+                strftime(cadena_tiempo,80,"%H:%M:%S", stTm);
+                  printf ("Recibido %s, envio %s\n", cadena_recibida,cadena_tiempo );
+            }
+            else if (strcmp(cadena_recibida,"DAYTIME") == 0)
+            {
+                strftime(cadena_tiempo,80,"%A, %d de %B de %Y;%H:%M:%S", stTm);
+                  printf ("Recibido %s, envio %s\n", cadena_recibida,cadena_tiempo );
+            }
+            
+       
+      
+      	/* ------------------------------------------------------------------
+				Devolvemos el n\ufffdmero incrementado al cliente
+				--------------------------------------------------------------------*/
+			int enviado = sendto (Descriptor_Servidor, &cadena_tiempo, sizeof(cadena_tiempo), 0,
+			(struct sockaddr *) &Cliente, longitudCliente);
+
+		}
+          
+
+
+
+
+    }
+
+
+    //Cierre del socket
+    close(Descriptor_Servidor);
+    
+    
+}
